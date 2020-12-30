03ddbdef697e251dd256433abf38eeabc164bcdb
your-nameSubrota <subrota.shuvro@gmail.com>
2020-12-30 23:43:04 +0600

    main adapter

--- app/src/main/java/subrota/shuvro/retrofitlivedatamvvm/View/Adapters/MainAdapter.java
+++ app/src/main/java/subrota/shuvro/retrofitlivedatamvvm/View/Adapters/MainAdapter.java
@@ -1,4 +1,20 @@
 package subrota.shuvro.retrofitlivedatamvvm.View.Adapters;
 
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import subrota.shuvro.retrofitlivedatamvvm.Model.UserDataClass.UserResponseDataSet;
+
 public class MainAdapter {
+    private Context context;
+    private List <UserResponseDataSet> dataSets = new ArrayList<>();
+
+    public MainAdapter(Context context, List<UserResponseDataSet> dataSets) {
+        this.context = context;
+        this.dataSets = dataSets;
+    }
+
+    public Class ViewHolder
 }
